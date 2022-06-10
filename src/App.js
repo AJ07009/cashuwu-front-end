b765ede2b787ddf6767afd016b80106bb9c6f7ae
Aidan Josias <aidan@pop-os.localdomain>
2022-06-10 04:29:35 +0200

    almost there v2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Home from "./pages/home/home";
 import Login from "./pages/login/login";
+// import '../styles/global.css'
 import {
     BrowserRouter as Router,
     Switch,
